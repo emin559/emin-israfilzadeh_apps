fd9a6ba43a3d6a3c0dbcca4dcb51518789c91a74
Emin <israfilzadehemin@gmail.com>
2020-02-10 18:59:11 +0400

    CalcApp1

---
+++ src/main/java/app/BasicCalculator.java
@@ -0,0 +1,16 @@
+package app;
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        System.out.println("Add first number:");
+        Scanner input = new Scanner(System.in);
+        int number1 = input.nextInt();
+        System.out.println("Add second number:");
+        int number2 = input.nextInt();
+        System.out.println("Sum is: " + (number1+number2) );
+        System.out.println("Substaraction is: " + (number1-number2) );
+        System.out.println("Dividing is: " + (number1/number2) );
+        System.out.println("multiplication is: " + (number1*number2) );
+    }
+}
