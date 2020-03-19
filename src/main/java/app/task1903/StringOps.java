d90fa252f64cbc6f8f61605c61db83dcbbf78b5f
Emin <israfilzadehemin@gmail.com>
2020-03-19 17:30:22 +0400

    bitwiseLowerUpper

--- src/main/java/app/task1903/StringOps.java
+++ src/main/java/app/task1903/StringOps.java
@@ -6,73 +6,93 @@ package app.task1903;
  * http://sticksandstones.kstrom.com/appen.html
  */
 public class StringOps {
-
+  // a-zA-Z ONLY
   // abc -> ABC
   // aBc -> AbC
   // Abc -> aBC
-  String invertCase(String origin) {
-    StringBuilder result = new StringBuilder();
-    for (int i = 0; i < origin.length(); i++) {
-      int lInt = origin.charAt(i);
-      if (lInt >= 97) {
-        char l = (char) (lInt - 32);
-        result.append(l);
-
-      } else {
-        char l = (char) (lInt + 32);
-        result.append(l);
-      }
-
+  String invertCaseV1(String origin) {
+    StringBuilder sb = new StringBuilder();
+    for (int i=0; i < origin.length(); i++) {
+      char c = origin.charAt(i);
+      if (c >= 'a') sb.append((char)(c-32));
+      else sb.append((char)(c+32));
     }
-    return result.toString();
+    return sb.toString();
+  }
+
+  String invertCaseV2(String origin) {
+    char[] chars = origin.toCharArray();
+    for (int i=0; i < chars.length; i++) {
+      char c = origin.charAt(i);
+      if (c >= 'a') chars[i] = (char)(c - 32);
+               else chars[i] = (char)(c + 32);
+    }
+    return new String(chars);
+  }
+
+  String invertCaseV3(String origin) {
+    char[] chars = origin.toCharArray();
+    for (int i=0; i < chars.length; i++) {
+      char c = origin.charAt(i);
+      chars[i] = (c >= 'a') ? (char)(c - 32) : (char)(c + 32);
+    }
+    return new String(chars);
+  }
+
+  String invertCaseV4(String origin) {
+    char[] chars = origin.toCharArray();
+    for (int i=0; i < chars.length; i++) {
+      chars[i] = (char) (chars[i] ^ 32); // bit XOR operation
+    }
+    return new String(chars);
   }
 
   // abc -> ABC
   // aBc -> ABC
   // Abc -> ABC
   String toUpperCase(String origin) { // a -> A A -> A
-    StringBuilder result = new StringBuilder();
-    for (int i = 0; i < origin.length(); i++) {
-      int lInt = origin.charAt(i);
-      if (lInt >= 97) {
-        char l = (char) (lInt - 32);
-        result.append(l);
-
-      } else {
-        char l = (char) lInt;
-        result.append(l);
-      }
-
+    StringBuilder sb = new StringBuilder();
+    for (int i=0; i < origin.length(); i++) {
+      char c = origin.charAt(i);
+      if (c >= 'a') sb.append((char)(c-32));
+      else sb.append(c);
     }
-    return result.toString();
+    return sb.toString();
+  }
+
+  String toUpperCaseV2(String origin) { // a -> A A -> A
+    char[] chars = origin.toCharArray();
+    for (int i=0; i < chars.length; i++) {
+      chars[i] = (char) (chars[i] & ~ 32); // bit XOR operation
+    }
+    return new String(chars);
   }
 
   // abc -> abc
   // aBc -> abc
   // Abc -> abc
   String toLowerCase(String origin) { // A -> a a -> a
-    StringBuilder result = new StringBuilder();
-    for (int i = 0; i < origin.length(); i++) {
-      int lInt = origin.charAt(i);
-      if (lInt >= 97) {
-        char l = (char) (lInt - 32);
-        result.append(l);
-
-      } else {
-        char l = (char) lInt;
-        result.append(l);
-      }
-
+    StringBuilder sb = new StringBuilder();
+    for (int i=0; i < origin.length(); i++) {
+      char c = origin.charAt(i);
+      if (c >= 'a') sb.append(c);
+      else sb.append((char)(c+32));
     }
-    return result.toString();
+    return sb.toString();
+  }
+
+  String toLowerCaseV2(String origin) { // A -> a a -> a
+    char[] chars = origin.toCharArray();
+    for (int i=0; i < chars.length; i++) {
+      chars[i] = (char) (chars[i] | 32);
+    }
+    return new String(chars);
   }
 
   public static void main(String[] args) {
-    StringOps ooo = new StringOps();
+    System.out.println(new StringOps().toUpperCaseV2( "HelloWorld"));
+    System.out.println(new StringOps().toLowerCaseV2( "HelloWorld"));
 
-    System.out.println(ooo.toUpperCase("EoIuYIjlkjI"));
-    System.out.println(ooo.toLowerCase("OIUPOIUREWOINMBVC"));
-    System.out.println(ooo.invertCase("UuOlIk"));
   }
 
 }
